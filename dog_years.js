1d3178ae415e04ff94a5934b67737bb71c16e0b5
harman31 <63211963+harman31@users.noreply.github.com>
2021-03-20 23:26:08 -0400

    Add files via upload

---
+++ dog_years.js
@@ -0,0 +1,16 @@
+//Current Age
+const myAge = 29;
+//Early Years
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Ed".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
